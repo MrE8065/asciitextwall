f43fab76041b2c96272d387b1fdd70db9d236d41
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2026-06-14 13:28:06 +0200

    Implemented arguments use - Control the script using arguments to set set text, font, color, etc

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
-import sys
+import argparse
 
 import pyfiglet
 from PIL import Image, ImageDraw, ImageFont, ImageText
 
-img_width = 1920
-img_height = 1080
-text_var = "Hello World"
-font_var = "big"
-mono_font = ""
-if sys.platform == "win32":
-    mono_font = "C:/Windows/Fonts/consola.ttf"
-elif sys.platform == "linux":
-    mono_font = "/usr/share/fonts/truetype/DejaVu/DejaVuSansMono.ttf"
-width = 200
-size = 20
+
+parser = argparse.ArgumentParser(
+    prog='asciitextwall',
+    description='Simple tool to generate ascii text wallpapers',
+)
+
+parser.add_argument('-t', '--text', help="Text to display", type=str, required=True)
+parser.add_argument('-f', '--font', help="Pyfiglet font to use", type=str, required=True)
+parser.add_argument('-mf', '--mono_font', help="Monospaced font to use", type=str, required=True)
+parser.add_argument('-s', '--size', help="Size of the text", type=int, required=True)
+parser.add_argument('-w', '--width', help="Width of the text. 80 by default", type=int)
+parser.add_argument('-c', '--color', help="Color of the text", type=str, required=True)
+parser.add_argument('-b', '--bg_color', help="Color of the background", type=str, required=True)
+parser.add_argument('-iw', '--img_width', help="Output image width", type=int, required=True)
+parser.add_argument('-ih', '--img_height', help="Output image height", type=int, required=True)
+
+args = parser.parse_args()
 
 
 def main():
-    image = Image.new(mode="RGB", size=(img_width, img_height), color="#000000")
-    ascii_art = pyfiglet.figlet_format(text_var, font=font_var, width=width)
+    image = Image.new(mode="RGB", size=(args.img_width, args.img_height), color=args.bg_color)
+    ascii_art = pyfiglet.figlet_format(args.text, font=args.font, width=args.width or 80)
     # A monospaced font is needed in this case, because the characters might be missaligned otherwise
-    font = ImageFont.truetype(font=mono_font, size=size)
+    font = ImageFont.truetype(font=args.mono_font, size=args.size)
     text = ImageText.Text(ascii_art, font)
 
     bbox = text.get_bbox()
     # Calculate the position for the text to be in the center of the image
-    x = (img_width - bbox[2]) // 2
-    y = (img_height - bbox[3]) // 2
+    x = (args.img_width - bbox[2]) // 2
+    y = (args.img_height - bbox[3]) // 2
 
     final = ImageDraw.Draw(image)
     # Write the text to the image
-    final.text((x, y), text, "#ffffff")
+    final.text((x, y), text, args.color)
 
     # Write the image to temp and open it
     image.show()
